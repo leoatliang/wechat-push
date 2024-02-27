534187a0fc0b64479cb46dfd4d1045860cec2e31
leoatliang <luguoliang@yahoo.com>
2024-02-27 16:07:30 +0800

    update

--- config/index.js
+++ config/index.js
@@ -1,24 +1,19 @@
 const params = {
     // 微信测试号获取
-    appid: 'wx1b945ee52d07dbde',
-    secret: '82a7c0f975f3a8d4eab76373e071a144',
-    touser: 'o0-3O6YPVavlz62wFwlMe-TAUjOY',
-    template_id: 'o6DQm8H2lr19Jr_caWHji_ol3M0nuFgyiaVNVb-CQEM',
+    appid: '',
+    secret: '',
+    touser: '',
+    template_id: '',
 
     // 天气 API
-    weather_app_id: '92695596',
-    weather_app_secret: 'pnfmwF01',
-    weather_city_id: '101220501',
+    weather_app_id: '',
+    weather_app_secret: '',
+    weather_city_id: '',
 
     // 土味情话 API
-    words_api_key: 'eaa85aa8ff76ed13cd2b62f32cb9d515',
+    words_api_key: '',
 };
 
 module.exports = {
     params
 }
-
-// xxxxxxxxxxxxxxxxxx
-// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-// xx-xxxxxxxxxxxxxxxxxx-xxxxxx
-// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx_xxxxxxxxxxx
